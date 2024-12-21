f1970704a3a8fa0273cde67e4e3394592b018ddc
amitkanjariya <amitkanjariya28@gmail.com>
2024-12-21 23:17:14 +0530

    fix issues and add loading component

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
-  }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
