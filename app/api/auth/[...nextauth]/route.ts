16162cadcf2f32fff827dc5ac3e390663292e445
amitkanjariya <amitkanjariya28@gmail.com>
2025-07-24 23:58:48 +0530

    feat: refactor authentication setup and move authOptions to a separate file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,6 @@
-import bcrypt from "bcrypt"
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import prisma from "@/app/libs/prismadb"
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/libs/auth"; // or "@/lib/auth"
 
-export const authOptions : AuthOptions = {
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: { label: "email", type: "text" },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                if(!credentials?.email || !credentials?.password){
-                    throw new Error('Please enter both email and password')
-                }   
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
-                if(!user || !user?.hashedPassword){
-                    throw new Error('Invalid credentials');
-                }
-                const isCorrectPassword = await bcrypt.compare(
-                    credentials.password,
-                    user.hashedPassword
-                );
-                if(!isCorrectPassword){
-                    throw new Error('Invalid credentials');
-                }
-                if (!user.isVerified) {
-                    throw new Error('Please verify your email before logging in');
-                }
-                return user;
-            }
-        })
-    ],
-    debug: process.env.NODE_ENV === 'development',
-    session: {
-        strategy: "jwt",
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-};
+const handler = NextAuth(authOptions);
 
-const handler = NextAuth(authOptions as AuthOptions);
-
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
